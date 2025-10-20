ecee965fb25bf52b1560abb6fc1d2ffe93c20b1b
vankaspr <msvankaspr@gmail.com>
2025-10-20 15:35:13 +0500

    🤰 Create main router

---
+++ src/api/__init__.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter
+from core.config import settings
+from .auth import router as auth_router
+
+router = APIRouter(
+    prefix=settings.api.prefix
+)
+
+router.include_router(auth_router)
