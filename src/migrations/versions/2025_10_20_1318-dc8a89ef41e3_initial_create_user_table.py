9a161d2826048ba112636327e47f58955624aa9a
vankaspr <msvankaspr@gmail.com>
2025-10-20 13:25:39 +0500

    🫱 Create first migration: User Table

---
+++ src/migrations/versions/2025_10_20_1318-dc8a89ef41e3_initial_create_user_table.py
@@ -0,0 +1,42 @@
+"""initial: create user table
+
+Revision ID: dc8a89ef41e3
+Revises:
+Create Date: 2025-10-20 13:18:22.426788
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "dc8a89ef41e3"
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.create_table(
+        "users",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("email", sa.String(length=100), nullable=False),
+        sa.Column("hashed_password", sa.String(length=255), nullable=False),
+        sa.Column("username", sa.String(length=30), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+    )
+    op.create_index(op.f("ix_users_email"), "users", ["email"], unique=True)
+    op.create_index(
+        op.f("ix_users_username"), "users", ["username"], unique=True
+    )
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.drop_index(op.f("ix_users_username"), table_name="users")
+    op.drop_index(op.f("ix_users_email"), table_name="users")
+    op.drop_table("users")
