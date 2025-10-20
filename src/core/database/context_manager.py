937fcb94ac787a61aef9d0e731c068fde919638d
vankaspr <msvankaspr@gmail.com>
2025-10-20 13:26:55 +0500

    🤩 Update context manager

--- src/core/database/context_manager.py
+++ src/core/database/context_manager.py
@@ -6,9 +6,6 @@ from core.database import db_helper, Base
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # startup
-    async with db_helper.engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-        # await conn.run_sync(Base.metadata.drop_all)
     yield
     # shutdown
     await db_helper.dispose()
