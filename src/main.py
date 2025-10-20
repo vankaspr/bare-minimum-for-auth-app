201a800ee59b549e5370baea3a0ca826b6a73023
vankaspr <msvankaspr@gmail.com>
2025-10-20 15:36:15 +0500

    🤱 Includde router to main app

--- src/main.py
+++ src/main.py
@@ -2,11 +2,14 @@ import uvicorn
 import logging
 from fastapi import FastAPI
 from core.database import lifespan
+from api import router as api_router
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 app = FastAPI(lifespan=lifespan)
+
+app.include_router(api_router)
 
 @app.get("/")
 async def root():
