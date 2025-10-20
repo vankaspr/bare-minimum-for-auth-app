3bd2d4bef8308d29b4234948402de7215e5bb329
vankaspr <msvankaspr@gmail.com>
2025-10-20 13:51:04 +0500

    🧘‍♀️ Create schemas

---
+++ src/core/database/schemas/user.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel, EmailStr, ConfigDict
+
+class UserBase(BaseModel):
+    email: EmailStr
+    username: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+class UserUpdate(BaseModel):
+    email: EmailStr | None = None
+    username: str | None = None
+    
+class UserResponse(UserBase):
+    id: int
+    is_active: bool
+    is_verified: bool
+    is_superuser: bool
+    
+    model_config = ConfigDict(from_attributes=True,)
+    
+class UserAdminResponse(UserResponse):
+    pass
