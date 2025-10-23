e09cc95c8a2388b6dad3c628e3454a28c89d8c0a
vankaspr <msvankaspr@gmail.com>
2025-10-23 15:29:59 +0500

    😵‍💫 Cretae new routers: logout ando refresh (plus update login)

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,17 +1,21 @@
 from exceptions import auth
 from fastapi import APIRouter, Depends
 from typing import Annotated
-from sqlalchemy.ext.asyncio import AsyncSession
+
 
 from core.database.schemas.user import UserCreate, UserLogin
 from core.database.schemas.auth import (
     VerifyEmail,
     ForgotPasswordRequest,
     ResetPasswordRequest,
+    RefreshTokenRequest
     )
-from core.database import db_helper
+
+from core.database.models import User
+
 from core.services.user import UserService
 from core.dependency.services import get_user_service
+from core.dependency.user import get_current_user
 
 from utilities.jwt_token import create_jwt_token
 
@@ -47,14 +51,19 @@ async def login(
     ],
 ):
     user = await user_service.authenticate(login_data.login, login_data.password)
+    
     access_token = create_jwt_token(data={
         "sub": user.id,
         "type": "access_token",
         "username": user.username,
         })
+    
+    refresh_token = await user_service.create_refresh_token(user.id)
+    
     return {
         "message": "Login successful 🙂‍↕️🤌",
         "access_token": access_token,
+        "refresh_token": refresh_token,
         "token_type": "bearer",
         "user_id": user.id,
     }
@@ -76,12 +85,29 @@ async def verify_email(
     
 
 # 😺 todo: create template for bridge page!!
-# todo: ando 🤏 finish other routers:
     
 
 @router.post("/logout")
-async def logout():
-    pass
+async def logout(
+    user: Annotated[
+        User,
+        Depends(get_current_user)
+    ],
+    user_service: Annotated[
+        UserService,
+        Depends(get_user_service)
+    ]
+):
+    """ 
+    Revokes all refresh tokens for the 
+    current user upon logout.
+    """
+    await user_service.revoke_refresh_token(user.id)
+    
+    return {
+        "message" : "Successfully logged out"
+    }
+
 
 @router.post("/forgot-password")
 async def forgot_password(
@@ -108,4 +134,41 @@ async def reset_password(
     await user_service.reset_password(request.token, request.password)
     return {
         "message": "Password reset successfully"
+    }
+    
+
+@router.post("/refresh")
+async def refresh(
+    request: RefreshTokenRequest,
+    user_service: Annotated[
+        UserService, 
+        Depends(get_user_service)
+        ],
+):
+    """ 
+    Issues new access and refresh tokens using a valid refresh token
+    """
+    
+    # found olf refresh token in DB and validate it 
+    token = await user_service.validate_refresh_token(request.refresh_token)
+    
+    token.is_revoked = True
+    await user_service.session.commit()
+    
+    # create new access token
+    access_token = create_jwt_token(
+        {
+            "sub": token.user_id,
+            "type": "access_token",
+        }
+    )
+    
+    # create new refresh token 
+    refresh_token = await user_service.create_refresh_token(token.user_id)
+    
+    return {
+        "message": "Update access ando refresh tokens 🥳",
+        "access_token": access_token,
+        "refresh_token": refresh_token,
+        "token_type": "bearer",
     }
