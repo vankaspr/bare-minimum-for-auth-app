8ff8627fbe331ec7d340ebfebfb532662430ad2f
vankaspr <msvankaspr@gmail.com>
2025-10-24 13:59:10 +0500

    🤩 Create admin router

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from core.config import settings
 from .auth import router as auth_router
 from .user import router as user_router
+from .admin import router as admin_router
 
 router = APIRouter(
     prefix=settings.api.prefix
@@ -9,3 +10,4 @@ router = APIRouter(
 
 router.include_router(auth_router)
 router.include_router(user_router)
+router.include_router(admin_router)
