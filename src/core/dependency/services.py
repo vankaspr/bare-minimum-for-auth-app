a7a17b6f9a7974c4d58afcecb637a3735d2fd717
vankaspr <msvankaspr@gmail.com>
2025-10-24 15:24:49 +0500

    🍄 Create admin service dependency

--- src/core/dependency/services.py
+++ src/core/dependency/services.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from typing import Annotated
 from core.database import db_helper
 from core.services.user import UserService
+from core.services.admin import AdminService
 
 async def get_user_service(
     background_task: BackgroundTasks,
@@ -15,3 +16,12 @@ async def get_user_service(
         session=session,
         background_task=background_task
         )
+    
+    
+async def get_admin_service(
+    session: Annotated[
+        AsyncSession,
+        Depends(db_helper.session_getter)
+    ],
+) -> AdminService:
+    return AdminService(session=session)
