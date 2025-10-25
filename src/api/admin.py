ff4890836781e5dff03f9af2f3c4252e0182649f
vankaspr <msvankaspr@gmail.com>
2025-10-25 11:45:36 +0500

    👏 Create patch router for admin: delete, deactivate, reactivate

--- src/api/admin.py
+++ src/api/admin.py
@@ -24,3 +24,47 @@ async def statistics(
     ],
 ):
     return await admin_service.get_user_stats()
+
+# router with -> {user_id}
+
+@router.patch("/deactivate/{user_id}")
+async def deactivate(
+    user_id: int,
+    user: Annotated[
+        User,
+        Depends(get_current_superuser)
+    ],
+    admin_service: Annotated[
+        AdminService,
+        Depends(get_admin_service)
+    ],
+):
+    return await admin_service.deactivate_user(user_id=user_id)
+
+@router.patch("/reactivate/{user_id}")
+async def reactivate(
+    user_id: int,
+    user: Annotated[
+        User,
+        Depends(get_current_superuser)
+    ],
+    admin_service: Annotated[
+        AdminService,
+        Depends(get_admin_service)
+    ],   
+):
+    return await admin_service.reactivate_user(user_id=user_id)
+
+@router.patch("/delete/{user_id}")
+async def delete(
+    user_id: int,
+    user: Annotated[
+        User,
+        Depends(get_current_superuser)
+    ],
+    admin_service: Annotated[
+        AdminService,
+        Depends(get_admin_service)
+    ],
+):
+    return await admin_service.delete_user(user_id=user_id)
