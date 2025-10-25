aa862c0b223df61ed19d61828192b65fdf7e923c
vankaspr <msvankaspr@gmail.com>
2025-10-25 14:17:05 +0500

    🥶 ADD CORS

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import uvicorn
 import logging
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from core.database import lifespan
 from api import router as api_router
 from api.views import router as views_router
@@ -9,6 +10,16 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "*"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api_router)
 app.include_router(views_router)
